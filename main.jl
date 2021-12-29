741af4a346ec1374750b98e5cb469afc677419d0
kozvojtex <vojtech.kozel.cz@gmail.com>
2021-12-29 11:00:55 +0100

    repaired some bugs

--- main.jl
+++ main.jl
@@ -1,6 +1,7 @@
 
 using DelimitedFiles
 using Random
+using Printf
 
 include("parzenwindow.jl")
 include("gmm.jl")
@@ -22,29 +23,34 @@ test_data_n = data_normal[:, Int(round(3*N_normal/4)):end]
 test_data_a = data_anomal[:, begin:Int(round(N_anomal/2))]
 N = size(data_normal)[1]
 
-logistic(x) = 1/(1+ℯ^(-x))
 
 function test_model(model, params, valid, test_n, test_a, q)
     pred = zeros(2, 3)
     for (j, data) in zip(1:3, [valid, test_n, test_a])
         for i=1:size(data)[2]
-            if logistic(model(params, data[:,i])) > q pred[1, j]+=1
+            if model(params, data[:,i]) >= q pred[1, j]+=1
             else pred[2, j]+=1 end
         end
     end
-    @show pred[1, 1]/sum(pred[:, 1]), pred[1, 2]/sum(pred[:, 2]), 1-pred[1, 3]/sum(pred[:, 3])
-    @show ((pred[1, 2]/sum(pred[:, 2]))*size(test_n)[2] + (pred[2, 3]/sum(pred[:, 3]))*size(test_a)[2])/(size(test_n)[2]+size(test_a)[2])
+    acc_valid = pred[1, 1]/sum(pred[:, 1])
+    acc_test_n = pred[1, 2]/sum(pred[:, 2])
+    acc_test_a = 1-pred[1, 3]/sum(pred[:, 3])
+    @printf("acc: valid %.2f, test_n %.2f, test_a %.2f", acc_valid, acc_test_n, acc_test_a)
+    @printf("acc: %.2f", (acc_test_n*size(test_n)[2]+acc_test_a*size(test_a)[2])/(size(test_n)[2]+size(test_a)[2]))
 end
 
 
-K = 50
+K = 3
 ps, gmm_model, gm_model = create_gmm(K, N); # prepare model
 EM!(ps, trn_data, K, gmm_model, gm_model, 100); # learn model params
 arr = []
 for i=1:size(valid_data)[2]
-    append!(arr, logistic(gmm_model(ps, valid_data[:, i])))
+    append!(arr, gmm_model(ps, valid_data[:, i]))
 end
-test_model(gmm_model, ps, valid_data, test_data_n, data_anomal, quantile!(arr, 0.1)); # run on valid data
-@show roc_auc(gmm_model, ps, hcat(test_data_n, data_anomal), 
-        vcat(ones(size(test_data_n)[2], 1), zeros(size(data_anomal)[2], 1)));
+test_model(gmm_model, ps, valid_data, test_data_n, data_anomal, quantile!(arr, 0.25));
+testing_data = hcat(test_data_n, data_anomal);
+testing_labels = vcat(ones(size(test_data_n)[2], 1), zeros(size(data_anomal)[2], 1));
+roc_auc(gmm_model, ps, testing_data, testing_labels);
+recall(gmm_model, ps, testing_data, testing_labels, quantile!(arr, 0.25));
+precision(gmm_model, ps, testing_data, testing_labels, quantile!(arr, 0.25));
 
--- roc.jl
+++ roc.jl
@@ -1,6 +1,6 @@
 
-using NumericalIntegration
 using Plots
+using EvalMetrics
 
 function get_probs(model, θ, data)
     probs = []
@@ -10,40 +10,24 @@ function get_probs(model, θ, data)
     return probs
 end
 
-function fpr_tpr(ŷ, y, ϵ)
-    fp = sum(map(x->x[1] == 1 && x[2] == 0, zip(map(x->x>ϵ, ŷ), y)))
-    tn = sum(map(x->x[1] == 0 && x[2] == 0, zip(map(x->x>ϵ, ŷ), y)))
-    fpr = fp/(fp+tn)
+function roc_auc(model, θ, data, targets)
+    scores = get_probs(model, θ, data)
+    auc = auc_trapezoidal(prcurve(targets, scores)...)
+    println("auc: ", auc)
+end
 
+function recall(model, θ, data, y, ϵ) 
+    ŷ = get_probs(model, θ, data)
     fn = sum(map(x->x[1] == 0 && x[2] == 1, zip(map(x->x>ϵ, ŷ), y)))
     tp = sum(map(x->x[1] == 1 && x[2] == 1, zip(map(x->x>ϵ, ŷ), y)))
     tpr = tp/(fn+tp)
-
-    return fpr, tpr
+    println("recall: ", tpr)
 end
 
-function roc_auc(model, θ, data, labels)
-    probs = get_probs(model, θ, data)
-    fpr_tpr_arr = []
-    for ϵ=0:((maximum(probs)-minimum(probs))/(size(data)[2]*maximum(probs))):1
-        fpr, tpr = fpr_tpr(probs, labels, ϵ)
-        append!(fpr_tpr_arr, [(fpr, tpr)])
-    end
-    sort!(fpr_tpr_arr)
-    
-    # trapezoid integration
-    auc = integrate(map(x->x[1], fpr_tpr_arr), 
-                    map(x->x[2], fpr_tpr_arr))
-    return auc
-end
-
-function rocplot(model, θ, data, labels)
-    probs = get_probs(model, θ, data)
-    fpr_tpr_arr = []
-    for ϵ=0:((maximum(probs)-minimum(probs))/size(data)[2]):1
-        fpr, tpr = fpr_tpr(probs, labels, ϵ)
-        append!(fpr_tpr_arr, [(fpr, tpr)])
-    end
-    sort!(fpr_tpr_arr)
-    plot(map(x->x[1], fpr_tpr_arr), map(x->x[2], fpr_tpr_arr))
+function precision(model, θ, data, y, ϵ) 
+    ŷ = get_probs(model, θ, data)
+    fp = sum(map(x->x[1] == 1 && x[2] == 0, zip(map(x->x>ϵ, ŷ), y)))
+    tp = sum(map(x->x[1] == 1 && x[2] == 1, zip(map(x->x>ϵ, ŷ), y)))
+    tpr = tp/(fp+tp)
+    println("precision: ", tpr)
 end
